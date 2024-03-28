5ec17256e206a19270eb44eb56fa200218130e93
rubenzhu <rubenzhu@hotmail.com>
2024-03-28 13:36:53 +0100

    Struct ressource -> class Element

--- objet/Element.hh
+++ objet/Element.hh
@@ -1,12 +1,11 @@
-using ui32 = uint32_t;
-using ui8 = uint8_t;
+#include <string>
+using uint32 = uint32_t;
+using uint8 = uint8_t;
 
 enum class Type { batiment, troupe};
 
 struct Ressource {
-	Metaux,
-	Bois,
-	Viande
+	uint32 Metaux, Bois, Viande;
 };
 
 
@@ -14,13 +13,11 @@ class Element {
 private:
 	std::string _nom;
 	Type _type;
-	ui32 _vie;
-	ui32 _pos;
-	ui8  _ere;
+	uint32 _vie;
+	uint32 _pos;
+	uint8  _ere;
 	Ressource _cout;
 
 public:
 
-
-
-}
+};
