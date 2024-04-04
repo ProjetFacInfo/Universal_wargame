8f2f0c9670c2b4b0dda520ccd542a9f2ec58b025
alaun <alaun@Linux.alaun.virtualbox.org>
2024-04-04 20:30:57 +0200

    ajout test

--- main.cc
+++ main.cc
@@ -1,7 +1,12 @@
 #include <iostream>
 
 #include "objet/Jeu.hh"
+#include "instance/carteInstanceTest.hh"
 
 int main(int argc, char *argv[]){
+
+    Jeu jeu(maps["1"]);
+    jeu.afficherCarte(std::cout);
+
     return 0;
 }
