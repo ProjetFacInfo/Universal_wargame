b0192dec4a8ce915a9fc3677fcc733e963c50dd6
rubenzhu <rubenzhu@hotmail.com>
2024-04-13 10:38:09 +0200

    Terrain.hh mask 32 bits

--- objet/Terrain.hh
+++ objet/Terrain.hh
@@ -1,5 +1,12 @@
 #pragma once
 
 enum class Terrain{
-    plaine, foret, colline, montagne, eau, desert
+    plaine = 1,
+    colline = 2,
+    montagne = 4,
+    foret = 8,
+    desert = 16,
+    eau = 32,
+    mer = 64,
+    ciel = 128
 };
