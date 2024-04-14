2f6c551a65140db33eb5301f51dc137ec39d5c23
alaun <alexis.launay@etud.univ-angers.fr>
2024-04-14 12:50:39 +0200

    ajout méthodes

--- objet/Joueur.cc
+++ objet/Joueur.cc
@@ -3,4 +3,40 @@
 const Ressources RESSOURCES_JOUEUR_INIT = {500, 500, 500};
 
 Joueur::Joueur(std::shared_ptr<Carte> const & carte, TypeJoueur const & joueur)
-    :_ressources(RESSOURCES_JOUEUR_INIT), _base(carte->getCase((joueur == TypeJoueur::joueur1) ? carte->getPosBase1() : carte->getPosBase2())._element){}
+    :_carte(carte), _ressources(RESSOURCES_JOUEUR_INIT), _base(carte->getCase((joueur == TypeJoueur::joueur1) ? carte->getPosBase1() : carte->getPosBase2())._element), _ere(0){}
+
+std::shared_ptr<Carte> const &Joueur::getCarte() const
+{
+    return _carte;
+}
+
+Ressources const &Joueur::getRessources() const
+{
+    return _ressources;
+}
+
+std::queue<BatimentRessource> const &Joueur::getBatiments() const
+{
+    return _batiments;
+}
+
+std::queue<Troupe> const &Joueur::getTroupes() const
+{
+    return _troupes;
+}
+
+std::shared_ptr<Element> const &Joueur::getBase() const
+{
+    return _base;
+}
+
+bool Joueur::aPerdu() const
+{
+    return _base->estDetruit();
+}
+
+bool Joueur::acheterBatiment(TypeRessource const &ressource, uint16_t i, uint16_t j)
+{
+    Ressources cout = dataBatimentRessources[ressource].cout;
+    if (cout * MULT_ERE[_ere] < _ressources)
+}
--- objet/Joueur.hh
+++ objet/Joueur.hh
@@ -10,10 +10,22 @@
 
 class Joueur{
 private:
+    std::shared_ptr<Carte> _carte;
     Ressources _ressources;
     std::queue<BatimentRessource> _batiments;
     std::queue<Troupe> _troupes;
     std::shared_ptr<Element> _base;
+    uint8_t _ere;
 public:
     Joueur(std::shared_ptr<Carte> const & carte, TypeJoueur const & joueur);
+    std::shared_ptr<Carte> const & getCarte() const;
+    Ressources const & getRessources() const;
+    std::queue<BatimentRessource> const & getBatiments() const;
+    std::queue<Troupe> const & getTroupes() const;
+    std::shared_ptr<Element> const & getBase() const;
+
+    bool aPerdu() const;
+
+    bool acheterBatiment(TypeRessource const & ressource, uint16_t i, uint16_t j);
+    bool acheterTroupe(TypeTroupe const & troupe, uint16_t i, uint16_t j);
 };
