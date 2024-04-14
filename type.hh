dd409ed4b76e987a4615eb322069e4138b94f992
rubenzhu <rubenzhu@hotmail.com>
2024-04-14 11:38:48 +0200

    Update type.hh for all file

--- type.hh
+++ type.hh
@@ -1,14 +1,25 @@
 #pragma once
 #include <cstdint>
 #include <bitset>
-#include <string>
-#include <map>
+
+
+using uint8 = std::uint8_t;
+using uint16 = std::uint16_t;
+using uint32 = std::uint32_t;
+using signature = std::bitset<16>;
+
+const uint8 NB_ERE = 5;
+
+enum class TypeRessource{
+    metaux, bois, viande
+};
+
+enum class TypeTroupe{
+    infanterie, archer, cavalier
+};
 
 
 // ECS
-using u32 = std::uint32_t;
 using entity = u32;
 const entity MAX_ENTITIES = 1000;
 using signature = std::bitset<32>;
-
-// TODO:const_map
