7f84efefbd9963a10aedcc5564bd089330852d99
rubenzhu <rubenzhu@hotmail.com>
2024-04-14 11:47:29 +0200

    Update Troupe with mult ere

--- objet/Troupe.cc
+++ objet/Troupe.cc
@@ -1,7 +1,7 @@
 #include "Troupe.hh"
 
-Troupe::Troupe(std::string const &nom, uint32 vie, uint32 pos, Ressource const &cout, TypeJoueur const & joueur)
-    :Element(nom,vie,pos,cout, joueur){}
+Troupe::Troupe(uint32 vie, uint32 pos, TypeJoueur const & joueur, TypeTroupe const & t)
+    :Element(vie,pos, joueur), _type(t){}
 
 TypeElement Troupe::type() const
 {
@@ -14,4 +14,12 @@ void Troupe::add_component(signature const & s, std::unique_ptr<Icomponent> C) {
 
 void Troupe::remove_component(signature const & s) {
     _components.erase(s);
+}
+
+std::string const & Troupe::nom() const override {
+    return dataTroupeRessources.at(_type).nom;
+}
+
+Ressource const & Troupe::cout() const override {
+    return (dataTroupeRessources.at(_type).cout * MULT_ERE[ere()]);
 }
