ca5c1bf10a7241fb87311923bad00a4ecf118435
rubenzhu <rubenzhu@hotmail.com>
2024-04-15 17:30:03 +0200

    type.hh fix merge conflit

--- type.hh
+++ type.hh
@@ -6,16 +6,9 @@
 using uint8 = std::uint8_t;
 using uint16 = std::uint16_t;
 using uint32 = std::uint32_t;
-using signature = std::bitset<16>;
-
-const uint8 NB_ERE = 5;
-
-enum class TypeTroupe{
-    infanterie, archer, cavalier
-};
+using signature = std::bitset<32>;
 
 
 // ECS
-using entity = uint32;
-const entity MAX_ENTITIES = 1000;
-using signature = std::bitset<32>;
+// using entity = uint32;
+// const entity MAX_ENTITIES = 1000;
