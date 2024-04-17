c64a1f4fe97edc574615eaa2da5c92f67d46afaf
alaun <alexis.launay@etud.univ-angers.fr>
2024-04-17 13:33:16 +0200

    ajout méthodes liés à l'achat de batiments + troupes

--- objet/Joueur.cc
+++ objet/Joueur.cc
@@ -15,17 +15,17 @@ Ressources const &Joueur::getRessources() const
     return _ressources;
 }
 
-std::queue<BatimentRessource> const &Joueur::getBatimentsRessources() const
+std::queue<std::shared_ptr<BatimentRessource>> const &Joueur::getBatimentsRessources() const
 {
     return _batimentsRessources;
 }
 
-std::queue<BatimentTroupe> const &Joueur::getBatimentsTroupes() const
+std::queue<std::shared_ptr<BatimentTroupe>> const &Joueur::getBatimentsTroupes() const
 {
     return _batimentsTroupes;
 }
 
-std::queue<Troupe> const &Joueur::getTroupes() const
+std::queue<std::shared_ptr<Troupe>> const &Joueur::getTroupes() const
 {
     return _troupes;
 }
@@ -45,8 +45,26 @@ bool Joueur::acheterBatimentRessource(TypeRessource const &ressource, uint16_t i
     std::shared_ptr<BatimentRessource> batiment = std::make_shared<BatimentRessource>(ressource, _carte->pos(i,j), _type, _ere);
     if (_carte->caseBatimentAdjacent(_type, i, j) && batiment->cout(_ere) <= _ressources){
         _carte->poseElement(batiment, i, j);
+        _batimentsRessources.push(batiment);
         _ressources -= batiment->cout(_ere);
         return true;
     }
+    return false;
+}
+
+bool Joueur::acheterBatimentTroupe(uint16_t i, uint16_t j)
+{
+    std::shared_ptr<BatimentTroupe> batiment = std::make_shared<BatimentTroupe>(_carte->pos(i,j), _type, _ere);
+    if (_carte->caseBatimentAdjacent(_type, i, j) && batiment->cout(_ere) <= _ressources){
+        _carte->poseElement(batiment, i, j);
+        _batimentsTroupes.push(batiment);
+        _ressources -= batiment->cout(_ere);
+        return true;
+    }
+    return false;
+}
+
+bool Joueur::acheterTroupe(TypeTroupe const &troupe, uint16_t i, uint16_t j)
+{
     return false;
 }
--- objet/Joueur.hh
+++ objet/Joueur.hh
@@ -13,9 +13,9 @@ class Joueur{
 private:
     std::shared_ptr<Carte> _carte;
     Ressources _ressources;
-    std::queue<BatimentRessource> _batimentsRessources;
-    std::queue<BatimentTroupe> _batimentsTroupes;
-    std::queue<Troupe> _troupes;
+    std::queue<std::shared_ptr<BatimentRessource>> _batimentsRessources;
+    std::queue<std::shared_ptr<BatimentTroupe>> _batimentsTroupes;
+    std::queue<std::shared_ptr<Troupe>> _troupes;
     std::shared_ptr<Element> _base;
     uint8_t _ere;
     TypeJoueur _type;
@@ -23,14 +23,14 @@ public:
     Joueur(std::shared_ptr<Carte> const & carte, TypeJoueur const & joueur);
     std::shared_ptr<Carte> const & getCarte() const;
     Ressources const & getRessources() const;
-    std::queue<BatimentRessource> const & getBatimentsRessources() const;
-    std::queue<BatimentTroupe> const & getBatimentsTroupes() const;
-    std::queue<Troupe> const & getTroupes() const;
+    std::queue<std::shared_ptr<BatimentRessource>> const & getBatimentsRessources() const;
+    std::queue<std::shared_ptr<BatimentTroupe>> const & getBatimentsTroupes() const;
+    std::queue<std::shared_ptr<Troupe>> const & getTroupes() const;
     std::shared_ptr<Element> const & getBase() const;
 
     bool aPerdu() const;
 
     bool acheterBatimentRessource(TypeRessource const & ressource, uint16_t i, uint16_t j);
-    bool acheterBatimentTroupe(TypeRessource const & ressource, uint16_t i, uint16_t j);
+    bool acheterBatimentTroupe(uint16_t i, uint16_t j);
     bool acheterTroupe(TypeTroupe const & troupe, uint16_t i, uint16_t j);
 };
